adbb8bbe6689495ba12175cf68a92b7c4015f4c2
Noalino <bgelineau.dev@gmail.com>
2023-12-04 11:01:22 +0100

    Create User & Friend struct

---
+++ FriendFace/Friend.swift
@@ -0,0 +1,13 @@
+//
+//  Friend.swift
+//  FriendFace
+//
+//  Created by Noalino on 04/12/2023.
+//
+
+import Foundation
+
+struct Friend {
+    var id: UUID
+    var name: String
+}
---
+++ FriendFace/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  FriendFace
+//
+//  Created by Noalino on 04/12/2023.
+//
+
+import Foundation
+
+struct User {
+    var id: UUID
+    var isActive: Bool
+    var name: String
+    var age: Int
+    var company: String
+    var email: String
+    var address: String
+    var about: String
+    var registered: Date
+    var tags: [String]
+    var friends: [Friend]
+}
